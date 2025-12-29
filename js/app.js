b505124467ea9c29f400af4bceb3f3f084bb93ac
Marsmallow0767 <eahesapdenizhan@gmail.com>
2025-12-29 12:54:51 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,43 +1,38 @@
+// Firebase CDN
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore, collection, addDoc, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// 🔥 Firebase config (SENİN PROJE)
 const firebaseConfig = {
   apiKey: "AIzaSyCWmJVnZ4LIB07cPM8t_Run-1indRrDm9s",
   authDomain: "pipstagram-5b98e.firebaseapp.com",
   projectId: "pipstagram-5b98e",
+  storageBucket: "pipstagram-5b98e.firebasestorage.app",
+  messagingSenderId: "894857861700",
+  appId: "1:894857861700:web:1d0c9028ed4125ecf43a11"
 };
 
+// Init
 const app = initializeApp(firebaseConfig);
-const auth = getAuth();
-const db = getFirestore();
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
-window.login = () => {
-  const email = email.value;
-  const password = password.value;
+// 🔐 Login kontrolü
+onAuthStateChanged(auth, user => {
+  if (!user) {
+    console.log("Kullanıcı giriş yapmadı");
+  } else {
+    console.log("Giriş yapıldı:", user.uid);
+  }
+});
 
-  signInWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      document.getElementById("login").style.display = "none";
-      document.getElementById("chat").style.display = "block";
-    })
-    .catch(err => alert(err.message));
-};
-
-window.sendMessage = async () => {
-  await addDoc(collection(db, "messages"), {
-    text: msgInput.value,
+// 🔔 Bildirim fonksiyonu
+export async function sendNotification(toUid, text) {
+  await addDoc(collection(db, "notifications", toUid, "items"), {
+    text,
     time: Date.now()
   });
-  msgInput.value = "";
-};
+}
 
-onSnapshot(collection(db, "messages"), snap => {
-  messages.innerHTML = "";
-  snap.forEach(doc => {
-    const div = document.createElement("div");
-    div.className = "msg";
-    div.innerText = doc.data().text;
-    messages.appendChild(div);
-  });
-});
+
