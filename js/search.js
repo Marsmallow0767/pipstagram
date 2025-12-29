e9e1094874c967b347b1d0de6da4006e4164c6ac
Marsmallow0767 <eahesapdenizhan@gmail.com>
2025-12-29 15:18:06 +0300

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,32 +1,27 @@
 import { db } from "./firebase.js";
-import {
-  collection,
-  query,
-  where,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { collection, query, where, getDocs } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 window.searchUser = async () => {
-  const qText = document.getElementById("searchInput").value;
-  const resultDiv = document.getElementById("results");
-  resultDiv.innerHTML = "";
+  const text = searchInput.value.toLowerCase();
+  results.innerHTML = "";
 
   const q = query(
     collection(db, "users"),
-    where("username", ">=", qText),
-    where("username", "<=", qText + "\uf8ff")
+    where("username", ">=", text),
+    where("username", "<=", text + "\uf8ff")
   );
 
   const snap = await getDocs(q);
-
   snap.forEach(doc => {
-    const u = doc.data();
-    resultDiv.innerHTML += `
-      <div style="padding:10px;border-bottom:1px solid #ddd;cursor:pointer"
-           onclick="openProfile('${doc.id}')">
-        <img src="${u.avatar || ''}" width="40" style="border-radius:50%">
-        <b>${u.username}</b>
+    results.innerHTML += `
+      <div onclick="openUser('${doc.id}')">
+        👤 ${doc.data().username}
       </div>
     `;
   });
+};
+
+window.openUser = uid => {
+  location.href = "user.html?uid=" + uid;
 };
