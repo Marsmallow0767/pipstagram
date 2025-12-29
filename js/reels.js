356ca3fa100dbd47b7ff0e4afa63ad82ba392bfc
Marsmallow0767 <eahesapdenizhan@gmail.com>
2025-12-29 13:22:56 +0300

    Update reels.js

--- js/reels.js
+++ js/reels.js
@@ -1,38 +1,16 @@
-const reels = [
-  "https://www.w3schools.com/html/mov_bbb.mp4",
-  "https://www.w3schools.com/html/movie.mp4"
-];
+import { collection, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { db } from "./app.js";
 
-let index = 0;
-const video = document.createElement("video");
-video.src = reels[index];
-video.autoplay = true;
-video.loop = true;
-video.muted = true;
-video.style.width = "100vw";
-video.style.height = "100vh";
-video.style.objectFit = "cover";
+const wrap = document.body;
 
-document.body.innerHTML = "";
-document.body.appendChild(video);
-
-let startY = 0;
-
-document.addEventListener("touchstart", e=>{
-  startY = e.touches[0].clientY;
+onSnapshot(collection(db,"reels"), snap=>{
+  wrap.innerHTML="";
+  snap.forEach(d=>{
+    wrap.innerHTML += `
+      <video src="${d.data().video}"
+        autoplay loop muted
+        style="width:100vw;height:100vh;object-fit:cover">
+      </video>
+    `;
+  });
 });
-
-document.addEventListener("touchend", e=>{
-  const endY = e.changedTouches[0].clientY;
-  if(startY - endY > 50) next();
-  if(endY - startY > 50) prev();
-});
-
-function next(){
-  index = (index+1)%reels.length;
-  video.src = reels[index];
-}
-function prev(){
-  index = (index-1+reels.length)%reels.length;
-  video.src = reels[index];
-}
