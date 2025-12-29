f06ab4009c418e409d690e97f03985c162277359
Marsmallow0767 <eahesapdenizhan@gmail.com>
2025-12-29 15:01:47 +0300

    Update reels.js

--- js/reels.js
+++ js/reels.js
@@ -1,47 +1,31 @@
-import { db, auth } from "./firebase.js";
-import {
-  collection,
-  onSnapshot,
-  addDoc
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { auth, db, storage } from "./firebase.js";
+import { collection, addDoc, onSnapshot } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { ref, uploadBytes, getDownloadURL } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-import {
-  ref,
-  uploadBytes,
-  getDownloadURL
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
-
-import { storage } from "./firebase.js";
-
-// 🎬 REELS YÜKLEME
 window.uploadReel = async () => {
-  const file = document.getElementById("reelFile").files[0];
+  const file = reelFile.files[0];
   if (!file) return alert("Video seç");
 
-  const videoRef = ref(storage, "reels/" + Date.now() + file.name);
-  await uploadBytes(videoRef, file);
-  const url = await getDownloadURL(videoRef);
+  const vRef = ref(storage, "reels/" + Date.now() + file.name);
+  await uploadBytes(vRef, file);
+  const url = await getDownloadURL(vRef);
 
-  await addDoc(collection(db, "reels"), {
+  await addDoc(collection(db,"reels"),{
     video: url,
     uid: auth.currentUser.uid,
     time: Date.now()
   });
 
-  alert("Reels yüklendi 🎉");
+  alert("Reels yüklendi 🎬");
 };
 
-// 🎬 REELS GÖSTER
-onSnapshot(collection(db, "reels"), snap => {
-  const reelsDiv = document.getElementById("reels");
-  reelsDiv.innerHTML = "";
-
-  snap.forEach(doc => {
-    reelsDiv.innerHTML += `
-      <div style="margin-bottom:20px">
-        <video src="${doc.data().video}" controls style="width:100%"></video>
-        <button onclick="toggleLike('${doc.id}')">❤️ Like</button>
-      </div>
+onSnapshot(collection(db,"reels"), snap => {
+  reels.innerHTML = "";
+  snap.forEach(d => {
+    reels.innerHTML += `
+      <video src="${d.data().video}" controls style="width:100%"></video>
     `;
   });
 });
