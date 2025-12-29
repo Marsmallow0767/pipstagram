e114df696e6d94e4033a838f1eb2293eec2bd1a2
Marsmallow0767 <eahesapdenizhan@gmail.com>
2025-12-29 14:27:13 +0300

    Update follow.js

--- js/follow.js
+++ js/follow.js
@@ -1,23 +1,12 @@
-import { auth, db } from "./app.js";
-import {
-  doc, setDoc, deleteDoc
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { doc,updateDoc,arrayUnion } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { auth, db } from "./firebase.js";
 
-window.followUser = async () => {
-  const target = targetUid.value;
-  if (!target) return;
-
-  await setDoc(doc(db, "follows", auth.currentUser.uid, "following", target), {
-    time: Date.now()
-  });
-
-  alert("Takip edildi");
-};
-
-window.unfollowUser = async () => {
-  const target = targetUid.value;
-  if (!target) return;
-
-  await deleteDoc(doc(db, "follows", auth.currentUser.uid, "following", target));
-  alert("Takip bırakıldı");
-};
+export async function follow(userId){
+ await updateDoc(doc(db,"users",userId),{
+  followers:arrayUnion(auth.currentUser.uid)
+ });
+ await updateDoc(doc(db,"users",auth.currentUser.uid),{
+  following:arrayUnion(userId)
+ });
+}
