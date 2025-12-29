95ae4d9414cae5e69f190847cb9de5e871215b84
Marsmallow0767 <eahesapdenizhan@gmail.com>
2025-12-29 15:17:32 +0300

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,5 +1,5 @@
 import { auth, db, storage } from "./firebase.js";
-import { doc, setDoc, getDoc } from
+import { doc, getDoc, setDoc } from
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 import { ref, uploadBytes, getDownloadURL } from
 "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
@@ -8,29 +8,30 @@ window.saveProfile = async () => {
   const username = document.getElementById("username").value;
   const file = document.getElementById("avatar").files[0];
 
-  let avatarUrl = "";
+  let avatarURL = "";
 
   if (file) {
-    const avatarRef = ref(storage, "avatars/" + auth.currentUser.uid);
-    await uploadBytes(avatarRef, file);
-    avatarUrl = await getDownloadURL(avatarRef);
+    const r = ref(storage, "avatars/" + auth.currentUser.uid);
+    await uploadBytes(r, file);
+    avatarURL = await getDownloadURL(r);
   }
 
   await setDoc(doc(db, "users", auth.currentUser.uid), {
     username,
-    avatar: avatarUrl
-  });
+    avatar: avatarURL
+  }, { merge: true });
 
   alert("Profil güncellendi ✅");
 };
 
-// PROFİLİ ÇEK
-window.loadProfile = async () => {
-  const snap = await getDoc(doc(db,"users",auth.currentUser.uid));
+async function loadProfile() {
+  const snap = await getDoc(doc(db, "users", auth.currentUser.uid));
   if (snap.exists()) {
-    document.getElementById("username").value = snap.data().username;
-    if (snap.data().avatar)
-      document.getElementById("avatarImg").src = snap.data().avatar;
+    username.value = snap.data().username || "";
+    avatarImg.src = snap.data().avatar || "";
   }
-};
+}
+
+window.onload = loadProfile;
+
 
