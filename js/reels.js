48fed484bdac3ba36ce19cf9e25af04a639f4e7b
Marsmallow0767 <eahesapdenizhan@gmail.com>
2025-12-29 14:52:34 +0300

    Update reels.js

--- js/reels.js
+++ js/reels.js
@@ -1,27 +1,49 @@
-import { storage, db, auth } from "./firebase.js";
-import { ref, uploadBytes, getDownloadURL } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
-import { addDoc, collection, onSnapshot } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { db, auth } from "./firebase.js";
+import {
+  collection,
+  onSnapshot,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+import {
+  ref,
+  uploadBytes,
+  getDownloadURL
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+import { storage } from "./firebase.js";
+
+// 🎬 REELS YÜKLEME
 window.uploadReel = async () => {
-  const file = reelFile.files[0];
-  const refVid = ref(storage, "reels/" + file.name);
-  await uploadBytes(refVid, file);
-  const url = await getDownloadURL(refVid);
+  const file = document.getElementById("reelFile").files[0];
+  if (!file) return alert("Video seç");
 
-  await addDoc(collection(db,"reels"), {
-    url,
-    uid: auth.currentUser.uid
+  const videoRef = ref(storage, "reels/" + Date.now() + file.name);
+  await uploadBytes(videoRef, file);
+  const url = await getDownloadURL(videoRef);
+
+  await addDoc(collection(db, "reels"), {
+    video: url,
+    uid: auth.currentUser.uid,
+    time: Date.now()
   });
+
+  alert("Reels yüklendi 🎉");
 };
 
-onSnapshot(collection(db,"reels"), snap => {
-  reels.innerHTML="";
-  snap.forEach(d=>{
-    reels.innerHTML += `
-      <video src="${d.data().url}" controls style="width:100%"></video>
+// 🎬 REELS GÖSTER
+onSnapshot(collection(db, "reels"), snap => {
+  const reelsDiv = document.getElementById("reels");
+  reelsDiv.innerHTML = "";
+
+  snap.forEach(doc => {
+    reelsDiv.innerHTML += `
+      <div style="margin-bottom:20px">
+        <video src="${doc.data().video}" controls style="width:100%"></video>
+        <button onclick="toggleLike('${doc.id}')">❤️ Like</button>
+      </div>
     `;
   });
 });
+
 
