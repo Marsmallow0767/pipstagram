5a9069f677f501f0f8d087dab7735d7ca99d7dbe
Marsmallow0767 <eahesapdenizhan@gmail.com>
2025-12-29 16:16:48 +0300

    Update dm.js

--- js/dm.js
+++ js/dm.js
@@ -1,29 +1,18 @@
-import { auth, db } from "./firebase.js";
-import {
-  collection, addDoc, query, where, onSnapshot
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+function send(){
+ db.collection("messages").add({
+  from:auth.currentUser.uid,
+  text:msg.value,
+  time:firebase.firestore.FieldValue.serverTimestamp()
+ })
+}
 
-const to = new URLSearchParams(location.search).get("uid");
+db.collection("messages")
+.orderBy("time")
+.onSnapshot(s=>{
+ chat.innerHTML=""
+ s.forEach(m=>{
+  chat.innerHTML+=`<p>${m.data().text}</p>`
+ })
+})
 
-window.sendMsg = async () => {
-  await addDoc(collection(db, "messages"), {
-    from: auth.currentUser.uid,
-    to,
-    text: msg.value,
-    time: Date.now()
-  });
-  msg.value = "";
-};
-
-const q = query(
-  collection(db, "messages"),
-  where("from", "in", [auth.currentUser.uid, to])
-);
-
-onSnapshot(q, snap => {
-  messages.innerHTML = "";
-  snap.forEach(d => {
-    messages.innerHTML += `<p>${d.data().text}</p>`;
-  });
-});
 
