941addd5b18c24d5f26d1d0010153415592178ae
Marsmallow0767 <eahesapdenizhan@gmail.com>
2025-12-29 14:43:34 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCWmJVnZ4LIB07cPM8t_Run-1indRrDm9s",
   authDomain: "pipstagram-5b98e.firebaseapp.com",
   projectId: "pipstagram-5b98e",
-  storageBucket: "pipstagram-5b98e.firebasestorage.app",
+  storageBucket: "pipstagram-5b98e.appspot.com",
   messagingSenderId: "894857861700",
   appId: "1:894857861700:web:1d0c9028ed4125ecf43a11"
 };
@@ -15,3 +16,5 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
+
