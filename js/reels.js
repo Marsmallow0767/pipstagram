64617c0b5a132e9a26f72b3e7b8f1d6e4fc2bb56
Marsmallow0767 <eahesapdenizhan@gmail.com>
2025-12-29 16:15:46 +0300

    Update reels.js

--- js/reels.js
+++ js/reels.js
@@ -1,33 +1,11 @@
-import { auth, db, storage } from "./firebase.js";
-import { collection, addDoc, onSnapshot } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { ref, uploadBytes, getDownloadURL } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+db.collection("posts").onSnapshot(s=>{
+ reels.innerHTML=""
+ s.forEach(v=>{
+  reels.innerHTML+=`
+   <video src="${v.data().image}" controls autoplay loop></video>
+  `
+ })
+})
 
-window.uploadReel = async () => {
-  const file = reelFile.files[0];
-  if (!file) return alert("Video seç");
-
-  const vRef = ref(storage, "reels/" + Date.now() + file.name);
-  await uploadBytes(vRef, file);
-  const url = await getDownloadURL(vRef);
-
-  await addDoc(collection(db,"reels"),{
-    video: url,
-    uid: auth.currentUser.uid,
-    time: Date.now()
-  });
-
-  alert("Reels yüklendi 🎬");
-};
-
-onSnapshot(collection(db,"reels"), snap => {
-  reels.innerHTML = "";
-  snap.forEach(d => {
-    reels.innerHTML += `
-      <video src="${d.data().video}" controls style="width:100%"></video>
-    `;
-  });
-});
 
 
