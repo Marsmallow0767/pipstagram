87be9e5354e5f9f23d564b2ac5acc71993dfe16c
Marsmallow0767 <eahesapdenizhan@gmail.com>
2025-12-29 13:12:15 +0300

    Update dm.js

--- js/dm.js
+++ js/dm.js
@@ -4,26 +4,42 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const box = document.getElementById("dmMessages");
-const input = document.getElementById("dmInput");
+
+function chatId(uid1, uid2) {
+  return [uid1, uid2].sort().join("_");
+}
 
 window.sendDM = async () => {
-  if (!input.value) return;
+  const to = dmTarget.value;
+  const text = dmInput.value;
+  if (!to || !text) return;
 
-  await addDoc(collection(db, "globalDM"), {
-    text: input.value,
-    uid: auth.currentUser.uid,
+  const cid = chatId(auth.currentUser.uid, to);
+
+  await addDoc(collection(db, "chats", cid, "messages"), {
+    text,
+    sender: auth.currentUser.uid,
     time: Date.now()
   });
 
-  input.value = "";
+  dmInput.value = "";
 };
 
-onSnapshot(
-  query(collection(db, "globalDM"), orderBy("time")),
-  snap => {
-    box.innerHTML = "";
-    snap.forEach(m => {
-      box.innerHTML += `<p>${m.data().text}</p>`;
-    });
-  }
-);
+window.loadDM = () => {
+  const to = dmTarget.value;
+  if (!to) return;
+
+  const cid = chatId(auth.currentUser.uid, to);
+
+  onSnapshot(
+    query(collection(db, "chats", cid, "messages"), orderBy("time")),
+    snap => {
+      box.innerHTML = "";
+      snap.forEach(m => {
+        box.innerHTML += `<p>${m.data().text}</p>`;
+      });
+    }
+  );
+};
+
+dmTarget.addEventListener("change", loadDM);
