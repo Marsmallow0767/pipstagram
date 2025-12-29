8f35e3edb8356b0e2ccfd2bd69416e4361629fe6
Marsmallow0767 <eahesapdenizhan@gmail.com>
2025-12-29 16:14:29 +0300

    Update search.js

--- js/search.js
+++ js/search.js
@@ -1,27 +1,12 @@
-import { db } from "./firebase.js";
-import { collection, query, where, getDocs } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-window.searchUser = async () => {
-  const text = searchInput.value.toLowerCase();
-  results.innerHTML = "";
-
-  const q = query(
-    collection(db, "users"),
-    where("username", ">=", text),
-    where("username", "<=", text + "\uf8ff")
-  );
-
-  const snap = await getDocs(q);
-  snap.forEach(doc => {
-    results.innerHTML += `
-      <div onclick="openUser('${doc.id}')">
-        👤 ${doc.data().username}
-      </div>
-    `;
-  });
-};
-
-window.openUser = uid => {
-  location.href = "user.html?uid=" + uid;
-};
+function search(){
+ db.collection("users")
+ .where("username",">=",q.value)
+ .where("username","<=",q.value+"\uf8ff")
+ .get()
+ .then(s=>{
+  res.innerHTML=""
+  s.forEach(u=>{
+   res.innerHTML+=`<p>${u.data().username}</p>`
+  })
+ })
+}
