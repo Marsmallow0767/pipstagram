9657f6a423686b55fee3f9c9b7d0b4cb0970c440
Marsmallow0767 <eahesapdenizhan@gmail.com>
2025-12-29 14:59:53 +0300

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,26 +1,36 @@
-import { db, auth } from "./firebase.js";
-import {
-  addDoc,
-  collection,
-  onSnapshot,
-  query,
-  orderBy
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { auth, db, storage } from "./firebase.js";
+import { doc, setDoc, getDoc } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { ref, uploadBytes, getDownloadURL } from
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
-window.sendMsg = async () => {
-  await addDoc(collection(db,"messages"), {
-    text: msg.value,
-    uid: auth.currentUser.uid,
-    time: Date.now()
+window.saveProfile = async () => {
+  const username = document.getElementById("username").value;
+  const file = document.getElementById("avatar").files[0];
+
+  let avatarUrl = "";
+
+  if (file) {
+    const avatarRef = ref(storage, "avatars/" + auth.currentUser.uid);
+    await uploadBytes(avatarRef, file);
+    avatarUrl = await getDownloadURL(avatarRef);
+  }
+
+  await setDoc(doc(db, "users", auth.currentUser.uid), {
+    username,
+    avatar: avatarUrl
   });
-  msg.value="";
+
+  alert("Profil güncellendi ✅");
 };
 
-const q = query(collection(db,"messages"), orderBy("time"));
-onSnapshot(q, snap => {
-  messages.innerHTML="";
-  snap.forEach(d=>{
-    messages.innerHTML += `<p>${d.data().text}</p>`;
-  });
-});
+// PROFİLİ ÇEK
+window.loadProfile = async () => {
+  const snap = await getDoc(doc(db,"users",auth.currentUser.uid));
+  if (snap.exists()) {
+    document.getElementById("username").value = snap.data().username;
+    if (snap.data().avatar)
+      document.getElementById("avatarImg").src = snap.data().avatar;
+  }
+};
 
