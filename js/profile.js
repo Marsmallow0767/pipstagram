ffd8c683ff3e7a325ef8a17384306497ba61f97a
Marsmallow0767 <eahesapdenizhan@gmail.com>
2025-12-29 12:51:32 +0300

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -1,34 +1,23 @@
-import { db, auth } from "./firebase.js";
-import {
-  doc,
-  getDoc,
-  getDocs,
-  collection,
-  query,
-  where,
-  setDoc,
-  deleteDoc
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "firebase/auth";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
 
-window.openProfile = async () => {
-  feed.style.display = "none";
-  reels.style.display = "none";
-  profile.style.display = "block";
+const auth = getAuth();
+const storage = getStorage();
+const db = getFirestore();
 
+window.uploadProfile = async () => {
+  const file = document.getElementById("photo").files[0];
   const uid = auth.currentUser.uid;
 
-  const u = await getDoc(doc(db, "users", uid));
-  profileName.textContent = u.data().username;
+  const imgRef = ref(storage, `profiles/${uid}`);
+  await uploadBytes(imgRef, file);
+  const url = await getDownloadURL(imgRef);
 
-  const posts = await getDocs(
-    query(collection(db, "posts"), where("user", "==", uid))
-  );
-  postCount.textContent = posts.size;
+  await setDoc(doc(db, "users", uid), {
+    photoURL: url
+  }, { merge: true });
 
-  const followers = await getDocs(
-    query(collection(db, "follows"), where("to", "==", uid))
-  );
-  followerCount.textContent = followers.size;
-
-  followBtn.style.display = "none";
+  alert("Profil foto güncellendi!");
 };
+;
